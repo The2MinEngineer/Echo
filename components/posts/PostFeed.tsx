957565434a13a40d3e770c8781b1be9d20dd281e
pheany <114763753+pheany@users.noreply.github.com>
2023-06-06 21:05:46 +0100

    PostFeed userId || ''

--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,5 +1,4 @@
 import usePosts from '@/hooks/usePosts';
-
 import PostItem from './PostItem';
 
 interface PostFeedProps {
@@ -7,12 +6,12 @@ interface PostFeedProps {
 }
 
 const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
-  const { data: posts = [] } = usePosts(userId);
+  const { data: posts = [] } = usePosts(userId || '');
 
   return (
     <>
       {posts.map((post: Record<string, any>) => (
-        <PostItem userId={userId} key={post.id} data={post} />
+        <PostItem userId={userId || ''} key={post.id} data={post} />
       ))}
     </>
   );
